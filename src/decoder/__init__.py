aa277b2d378fc8831051c953151588091bbaba05
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2024-01-28 13:47:02 -0300

    move decoder logic to __init__

--- src/decoder/__init__.py
+++ src/decoder/__init__.py
@@ -1,1 +1,17 @@
-from ._parser import decode
+from typing import Iterator
+
+from src.classifier import token as tk
+
+from . import _parser
+
+def parse_tokens(it: Iterator[tk.Token]):
+    for token in it:
+        if token.kind == tk.Kind.Comment:
+            continue
+
+        if token.kind == tk.Kind.Label:
+            yield token.content[:-1], _parser.parse_label(it)
+
+
+def decode(it):
+    return {key: val for key, val in parse_tokens(it)}
