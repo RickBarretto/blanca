eebd2107d687e0bc9c21a82045e41c05b08f9b53
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2024-01-28 18:09:36 -0300

    [test\e2e]: add special case test for `:integer`

--- tests/e2e/types/test_integers.py
+++ tests/e2e/types/test_integers.py
@@ -12,3 +12,28 @@ def test_simple_integer_starting_with_0():
     so, they must be truncated at decoder level.
     """
     assert {"number": 1010} == src.load("number: 0000001010")
+
+def test_integer_followed_by_dot():
+    """Integer followed by dot should be considered as an :integer.
+
+    Differently from Python, that this would be considered a float,
+    in arturo, this is considered an ``:integer``.
+
+    Have a look at the examples bellow:
+    
+    Example
+    =======
+
+    Arturo's Repl
+    -------------
+
+        $> inspect 1.
+        1 :integer
+
+    Python's Repl
+    -------------
+
+        >>> 1.
+        1.0
+    """
+    assert {"number": 1} == src.load("number: 1.")
